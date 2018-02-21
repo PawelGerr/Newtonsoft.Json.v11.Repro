3933adeaba933420932f9a30b81adf78b810de7c
Pawel Gerr <pawel.gerr@thinktecture.com>
2018-02-21 14:57:54 +0100

    Initial commit

---
+++ JsonRepro10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Moq;
+using Newtonsoft.Json;
+using SharedLib;
+
+namespace JsonRepro10
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var mock = new Mock<IFoo>();
+			IFoo foo = mock.Object;
+
+			var json = JsonConvert.SerializeObject(foo, new JsonSerializerSettings() { Converters = { new FooConverter() } });
+			Console.WriteLine(json);
+		}
+	}
+}
---
+++ SharedLib/FooConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+using Newtonsoft.Json;
+
+namespace SharedLib
+{
+	public class FooConverter : JsonConverter
+	{
+		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+		{
+			writer.WriteValue("foo");
+		}
+
+		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+		{
+			return new Foo();
+		}
+
+		public override bool CanConvert(Type objectType)
+		{
+			return typeof(IFoo).GetTypeInfo().IsAssignableFrom(objectType);
+		}
+	}
+}
